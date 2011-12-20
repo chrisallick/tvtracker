ec9485ac553f0e4a38def28a0050b494ca213a42
Chris Allick <chrisallick@gmail.com>
2011-12-19 21:00:51 -0500

    setting up tornado

---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import json
+import os
+import logging
+
+import pymongo
+
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+##########   /*     */   ##########
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+##########   /*     */   ##########
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        # c = pymongo.Connection('localhost')
+        # db = c.shows
+        # shows = list()
+        # for a in db.shows.find().limit(10):
+        #     shows.append( a )
+
+        self.render( "index.html", shows=shows )
+
+
+##########   /*     */   ##########
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
